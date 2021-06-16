0298283927f87d42ea7023ee7b3c7cd3169d9136
Paul <iamtheodo@gmail.com>
2021-06-16 10:06:26 +1000

    Dual Display - To Be Tested

---
+++ VideoSwitchTest/Assets/Scripts/ToggleRenderText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class ToggleRenderText : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public RawImage rawImage;
+    public RenderTexture rtLow;
+    public RenderTexture rtHigh;
+
+    private bool _Toggle;
+
+    private void Start()
+    {
+        videoPlayer.targetTexture = rtLow;
+        rawImage.texture = rtLow;
+
+        Debug.Log("Press SPACE to toggle render textures...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _Toggle = !_Toggle;
+
+            if (_Toggle)
+            {
+                videoPlayer.targetTexture = rtHigh;
+                rawImage.texture = rtHigh;
+                Debug.Log("Set to HIGH");
+            }
+            else
+            {
+                videoPlayer.targetTexture = rtLow;
+                rawImage.texture = rtLow;
+                Debug.Log("Set to LOW");
+            }
+        }
+    }
+}
